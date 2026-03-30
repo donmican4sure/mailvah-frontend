0830f80c39cf0b4fa62d5947df92892209fd5bcb
donmican4sure <donmicanhq@gmail.com>
2026-03-30 08:45:34 +0100

    Add rewrites for supa-proxy to Supabase

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/supa-proxy/:path*',
+        destination: 'https://kihrduzknogrclvliewh.supabase.co/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
